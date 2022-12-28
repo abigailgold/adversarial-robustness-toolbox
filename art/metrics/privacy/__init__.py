4190ec69b0ffd0cd403a7f9af6615f3c0bf6baec
abigailt <abigailt@il.ibm.com>
2022-12-28 17:07:51 +0200

    Add comparison type to PDTP metric, fix SHAPr implementation so not dependent on k, fix tests.
    Signed-off-by: abigailt <abigailt@il.ibm.com>

--- art/metrics/privacy/__init__.py
+++ art/metrics/privacy/__init__.py
@@ -1,4 +1,4 @@
 """
 Module providing metrics and verifications.
 """
-from art.metrics.privacy.membership_leakage import PDTP, SHAPr
+from art.metrics.privacy.membership_leakage import PDTP, SHAPr, ComparisonType
